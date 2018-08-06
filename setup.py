b59fc3c74ca44e29d4ce30970645281833442b9d
Franklin Kingma <franklin@fourdigits.nl>
2018-08-06 12:49:07 +0300

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-wufoo',
-    version='0.3',
+    version='0.4.dev0',
     packages=['django_wufoo', 'django_wufoo.management', 'django_wufoo.management.commands'],
     include_package_data=True,
     license='MIT License',
