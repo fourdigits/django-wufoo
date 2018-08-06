87e1e2dba7b6bd74f3857b4d01bd7b5893a77ffe
Franklin Kingma <franklin@fourdigits.nl>
2018-08-06 12:46:33 +0300

    Preparing release 0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-wufoo',
-    version='0.3.dev0',
+    version='0.3',
     packages=['django_wufoo', 'django_wufoo.management', 'django_wufoo.management.commands'],
     include_package_data=True,
     license='MIT License',
